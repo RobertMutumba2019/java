33d5e44b35d92678783b8ba6895021b8f83a7b56
mutumba <rmutumba08@gmail.com>
2025-08-28 08:21:12 +0300

    first commit

---
+++ intro.java
@@ -0,0 +1,22 @@
+public class intro {
+    static String name ="Ken";
+    
+
+    public static void main(String[] args) {
+       String name = "Ken";
+   int mynum=54;
+    float num=10.12f;
+    float results=  mynum + num;
+
+     
+
+        System.out.println("Hello world " + name);
+        System.out.println("I am blessed");
+        System.out.println("I will be okay");
+        System.out.println(6*6);
+         System.out.println((6*6)/4+4);
+          System.out.println(results);
+         
+
+    }
+}
