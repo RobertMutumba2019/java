dc1304f500d73a72b2d5c8e1e2100bba3ec8461e
mutumba <rmutumba08@gmail.com>
2025-09-17 22:00:27 +0300

    Working on scanners

---
+++ pckage.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//Bulit in packages 
+public class pckage {
+    public static void main(String[] args) {
+        Scanner myOj = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = myOj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
